8ce58fecb2bb65c688b08499b22973fd9cd7724f
Abdulkadir <uabdulkadir06@gmail.com>
2024-04-18 12:01:03 +0100

    latest push 12:00

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -6,7 +6,7 @@ import useShowToast from "../../hooks/useShowToast";
 import useAuthStore from "../../store/authStore";
 import { setDoc, doc, getDoc } from "firebase/firestore";
 const GoogleAuth = ({prefix}) => {
-  const [signInWithGoogle, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, , ,error] = useSignInWithGoogle(auth);
   const showToast = useShowToast();
   const loginUser = useAuthStore((state) => state.login);
   const handleGoogleAuth = async () =>{
